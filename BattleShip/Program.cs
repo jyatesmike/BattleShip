3fec5e4b77c4f8e4240f32cee39423b916a81fb0
Michael <j.michael.yates@gmail.com>
2018-04-21 01:08:14 -0500

    formatting

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace BattleShip
 {
     class Program
     {
+        private const int XSIZE = 10;
+        private const int YSIZE = 10;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create ships
+            List<Ship> ships = new List<Ship>();
+
+            ships.Add(new Ship("Cruiser", 5, 'C'));
+            ships.Add(new Ship("BattleShip", 4, 'B'));
+            ships.Add(new Ship("Destroyer", 3, 'D'));
+            ships.Add(new Ship("Submarine", 3, 'S'));
+            ships.Add(new Ship("Tug", 2, 'T'));
+
+            //Setup battleship grid
+            BattleShipGrid grid = new BattleShipGrid(XSIZE, YSIZE, ships, true);
+
+            //Place ships on grid
+            grid.placeShips();
+
+            //Display grid to console
+            grid.display();
+
         }
     }
 }
--- BattleShip/Ship.cs
+++ BattleShip/Ship.cs
@@ -3,8 +3,30 @@ namespace BattleShip
 {
     public class Ship
     {
-        public Ship()
+        private int size = 0;
+        private char marker;
+        private String name;
+
+        public Ship(String name, int size, char marker)
         {
+            this.size = size;
+            this.marker = marker;
+            this.name = name;
+        }
+
+        public char getMarker()
+        {
+            return this.marker;
+        }
+
+        public int getSize()
+        {
+            return this.size;
+        }
+
+        public String getName()
+        {
+            return this.name;
         }
     }
 }
